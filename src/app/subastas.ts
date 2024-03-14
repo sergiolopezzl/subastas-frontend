03bcdb7a846e7376842048eebee93e60fe730201
sergiolopezzl <slopez1660@gmail.com>
2024-03-14 02:04:05 -0500

    v2

--- src/app/subastas.ts
+++ src/app/subastas.ts
@@ -1,3 +1,11 @@
 export class Subastas {
-
+  id: number;
+  nombre: string;
+  descripcion: string;
+  usuario: string;
+  cantidad: number;
+  evento: string;
+  precioInicial: number;
+  precioActual: number;
+  activa: boolean;
 }
